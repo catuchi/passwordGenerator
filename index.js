2c5a079aedca71f3e13d015d2a412c8ba558d834
Chibu <atuchibueze@gmail.com>
2022-08-24 11:21:24 -0400

    create DOM element & generate password event listener

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+// DOM elements
+const resultEL = document.getElementById("result");
+const lengthEL = document.getElementById("length");
+const uppercaseEL = document.getElementById("uppercase");
+const lowercaseEL = document.getElementById("lowercase");
+const numbersEL = document.getElementById("numbers");
+const symbolsEL = document.getElementById("symbols");
+const generateEL = document.getElementById("generate");
+const clipboardEL = document.getElementById("clipboard");
+
+generateEL.addEventListener("click", () => {
+  const length = +lengthEL.value;
+  const hasLower = lowercaseEL.checked;
+  const hasUpper = uppercaseEL.checked;
+  const hasNumber = numbersEL.checked;
+  const hasSymbol = symbolsEL.checked;
+
+  resultEL.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
 // Generator functions
 
 function getRandomLower() {
@@ -16,5 +49,3 @@ function getRandomSymbol() {
   const symbols = "!@#$%^&*(){}[]=<>/,.";
   return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomSymbol());
